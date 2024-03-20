c700854ff63812330608e08d7c87fc6c4b657a58
EnzoFerreira <enzoraci21@gmail.com>
2024-03-20 17:16:51 -0300

    feat: new Views introduced and used

--- project/marketplace/views.py
+++ project/marketplace/views.py
@@ -1,31 +1,29 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from marketplace.models import *
 from marketplace.forms import ItemForm
+from django.views import View
+from django.views.generic import ListView,CreateView,DetailView
 
-def market_view(request):
-    weapons = Weapon.objects.all().order_by('skin_name')
-    search_filter = request.GET.get('Search')
+class WeaponsListView(ListView):
+    model = Weapon
+    template_name = 'market_index.html'
+    context_object_name = 'weapons'
 
-    if search_filter:
-        weapons = Weapon.objects.filter(skin_name__icontains=search_filter)
+    def get_queryset(self):
+        weapons = super().get_queryset().order_by('skin_name')
+        search = self.request.GET.get('Search')
+        if search:
+            weapons = weapons.filter(skin_name__icontains=search)
+        return weapons
+class new_market_item(CreateView):
+    model = Weapon
+    form_class = ItemForm
+    template_name = 'new_item.html'
+    success_url = '/csmarket/'
 
-    return render(
-        request,
-        'market_index.html',
-        {'weapons': weapons},
-        )
-
-def new_item(request):
-    if request.method == 'POST':
-        new_item_form = ItemForm(request.POST, request.FILES)
-        if new_item_form.is_valid():
-            new_item_form.save()
-            return redirect('weapons_list')
-    else:
-        new_item_form = ItemForm()
-
-    return render(
-        request,
-        'new_item.html',{'new_item_form':new_item_form}
-    )
+class Weapon_View(DetailView):
+    model = Weapon
+    template_name = 'weapon_view.html'
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -23,10 +7,11 @@ from accounts.views import *
 
 
 urlpatterns = [
-    path('/new_item/',new_item, name='new_item'),
-    path('csmarket/',market_view, name='weapons_list'),
+    path('csmarket/',WeaponsListView.as_view(), name='weapons_list'),
     path('register/', register_view, name="register"),
+    path('/new_item/',new_market_item.as_view(), name='new_item'),
     path('login/', login_view , name='login'),
     path('logout/', logout_view, name='logout'),
+    path('csmarket/<int:pk>/',Weapon_View.as_view() ,name="weapon_details"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
