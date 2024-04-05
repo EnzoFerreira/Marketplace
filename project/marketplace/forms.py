4566feed8f1321594dc19bbe9050584e4a293f19
EnzoRaci <enzo.raci@scicrop.com>
2024-04-05 12:30:47 -0300

    Feat: float and weapon skin details added

--- project/marketplace/forms.py
+++ project/marketplace/forms.py
@@ -4,10 +4,20 @@ from marketplace.models import *
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Weapon
-        fields = ('skin_name','weapon_type','description','price','image')
+        fields = ('skin_name','skin_type','weapon_type','exterior','float','description','price','image')
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
         if price <= 0:
             self.add_error('price', 'Item price cannot be 0 or negative')
         return price
+    def clean_exterior(self):
+        exterior = self.cleaned_data.get('exterior')
+        if exterior == '':
+            self.add_error('exterior', 'Please enter the exterior of the item')
+        return exterior
+    def clean_float(self):
+        float = self.cleaned_data.get('float')
+        if float == None:
+            self.add_error('price', 'Item float cannot be null')
+        return float
