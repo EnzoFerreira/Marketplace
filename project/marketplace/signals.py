06c1e0f7fae7eb08604d29a175b5e436e5809f8c
EnzoFerreira <enzoraci21@gmail.com>
2024-04-25 09:04:50 -0300

    Signals fixed

--- project/marketplace/signals.py
+++ project/marketplace/signals.py
@@ -1,7 +1,12 @@
+import urllib.request
 from django.db.models.signals import *
 from django.dispatch import receiver
 from marketplace.models import *
 from django.db.models import Sum
+from django.shortcuts import get_object_or_404
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
 
 def weapon_update_inventory():
     weapon_count = Weapon.objects.all().count()
@@ -18,9 +23,91 @@ def weapon_update_inventory():
 def weapon_pre_save(sender, instance, **kwargs): 
     if not instance.description:
         #TODO
-        #FInd a way to reference the model name[]
-        #URL request to weapon description[]
-        instance.description = 'TBC'
+        #If´s for classes and specific weapons
+        weapon_name = instance.weapon_name.replace(' ', '-')
+        
+        weapon_name_final = f'{weapon_name}'.upper()
+
+        if 'KNIFE' in weapon_name_final:
+            
+            knife_name = weapon_name_final.capitalize()
+            if 'knife' in knife_name:
+                replace1 = knife_name.replace('k','K')
+                replace2 = replace1.replace('-','_')
+
+            url = f'https://liquipedia.net/counterstrike/{replace2}'
+            request = requests.get(url)
+            html_request = html.fromstring(request.content)
+
+            description = []
+
+            for i in html_request.xpath('//*[@id="mw-content-text"]/div/blockquote/div[1]'):
+                response = i.text_content().strip()
+                if response != '':
+                    description.append(response)
+                else: 
+                    weapon_name = weapon_name_final.lower()
+                    url = f'https://wiki.cs.money/weapons/{weapon_name}'
+                    request = requests.get(url)
+                    html_request = html.fromstring(request.content)
+                    
+                    for i in html_request.xpath('//*[@id="skins"]/div[1]/div/div[3]/div/div'):
+                        response = i.text_content().strip()
+                        if response:
+                            description.append(response)
+                instance.description = description[0]
+
+        if 'GALIL' in weapon_name_final:
+            url = f'https://liquipedia.net/counterstrike/Galil_AR'
+            request = requests.get(url)
+            html_request = html.fromstring(request.content)
+
+            description = []
+
+            for i in html_request.xpath('//*[@id="mw-content-text"]/div/blockquote/div[1]'):
+                response = i.text_content().strip()
+                if response:
+                    description.append(response)
+                instance.description = description[0]
+
+        if 'SHADOW' in weapon_name_final:
+            url = f'https://liquipedia.net/counterstrike/Shadow_Daggers'
+            request = requests.get(url)
+            html_request = html.fromstring(request.content)
+
+            description = []
+
+            for i in html_request.xpath('//*[@id="mw-content-text"]/div/blockquote/div[1]'):
+                response = i.text_content().strip()
+                if response:
+                    description.append(response)
+                instance.description = description[0]
+        else:
+            url = f'https://liquipedia.net/counterstrike/{weapon_name_final}'
+            request = requests.get(url)
+            html_request = html.fromstring(request.content)
+
+            description = []
+
+            for i in html_request.xpath('//*[@id="mw-content-text"]/div/blockquote/div[1]'):
+                response = i.text_content().strip()
+                if response != '':
+                    description.append(response)
+                instance.description = description[0]
+            
+            if description == []:
+                weapon_name = weapon_name_final.lower()
+                url = f'https://wiki.cs.money/weapons/{weapon_name}'
+                request = requests.get(url)
+                html_request = html.fromstring(request.content)
+                
+                for i in html_request.xpath('//*[@id="skins"]/div[1]/div/div[3]/div/div'):
+                    response = i.text_content().strip()
+                    if response:
+                        description.append(response)
+                    instance.description = description[0]
+
+
 
 
 @receiver(post_save, sender = Weapon)
